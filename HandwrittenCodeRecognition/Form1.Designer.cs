396a1635b8c561de45ca82e28c08744d3bad0346
Pranav Chadha <pranavchadha97@gmail.com>
2014-10-20 12:05:06 +0530

    Project added

---
+++ HandwrittenCodeRecognition/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace HandwrittenCodeRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.upload = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.normalise = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 62);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(644, 655);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(717, 62);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(620, 655);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // upload
+            // 
+            this.upload.Location = new System.Drawing.Point(985, 12);
+            this.upload.Name = "upload";
+            this.upload.Size = new System.Drawing.Size(75, 23);
+            this.upload.TabIndex = 3;
+            this.upload.Text = "Upload";
+            this.upload.UseVisualStyleBackColor = true;
+            this.upload.Click += new System.EventHandler(this.upload_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // normalise
+            // 
+            this.normalise.Location = new System.Drawing.Point(82, 23);
+            this.normalise.Name = "normalise";
+            this.normalise.Size = new System.Drawing.Size(75, 23);
+            this.normalise.TabIndex = 4;
+            this.normalise.Text = "Normalize";
+            this.normalise.UseVisualStyleBackColor = true;
+            this.normalise.Click += new System.EventHandler(this.normalise_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1349, 729);
+            this.Controls.Add(this.normalise);
+            this.Controls.Add(this.upload);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button upload;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button normalise;
+    }
+}
+
---
+++ HandwrittenCodeRecognition/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+
+namespace HandwrittenCodeRecognition
+{
+    public partial class Form1 : Form
+    {
+        Image<Gray, byte> first;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void open()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                first = new Image<Gray, byte>(openFileDialog1.FileName);
+                imageBox1.Image = first;
+            }
+        }
+
+        private void upload_Click(object sender, EventArgs e)
+        {
+            open();
+        }
+
+        private void normalise_Click(object sender, EventArgs e)
+        {
+            if (first == null)
+            {
+                open();
+            }
+
+            float x = first.Height / first.Width;
+            Image<Gray, byte> final = new Image<Gray, byte>(720, (int)(x*720));
+            
+            CvInvoke.cvResize(first, final, Emgu.CV.CvEnum.INTER.CV_INTER_AREA);
+            final = final.Not();
+            Image<Gray, byte> bg = new Image<Gray, byte>(final.Width, final.Height);
+            IntPtr se = CvInvoke.cvCreateStructuringElementEx(9, 9, 5, 5, Emgu.CV.CvEnum.CV_ELEMENT_SHAPE.CV_SHAPE_ELLIPSE, IntPtr.Zero);
+            CvInvoke.cvErode(final, bg, se, 1);
+
+            Image<Gray, byte> foreground = final - bg;
+            Image<Gray, byte> thresh = new Image<Gray,byte>(foreground.Width, foreground.Height);
+            CvInvoke.cvThreshold(foreground, thresh , 0, 255, Emgu.CV.CvEnum.THRESH.CV_THRESH_OTSU);
+            
+            
+
+            imageBox1.Image = final;
+            imageBox2.Image = final-bg;
+        }
+
+        
+    }
+}
